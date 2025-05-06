d93777d56937a24ab774f3247167ed3356d9be58
Sondre S Talleraas <sosttal@outlook.com>
2025-05-06 17:01:13 +0200

    restructuring and tidying

--- zip_fetcher.py
+++ zip_fetcher.py
@@ -9,13 +9,7 @@ from rich.progress import track
 from rich.prompt import Prompt
 from rich.panel import Panel
 from rich.table import Table
-
-LOGO = """███████╗██╗██████╗       ███████╗███████╗████████╗ ██████╗██╗  ██╗███████╗██████╗   TM
-╚══███╔╝██║██╔══██╗      ██╔════╝██╔════╝╚══██╔══╝██╔════╝██║  ██║██╔════╝██╔══██╗
-  ███╔╝ ██║██████╔╝█████╗█████╗  █████╗     ██║   ██║     ███████║█████╗  ██████╔╝
- ███╔╝  ██║██╔═══╝ ╚════╝██╔══╝  ██╔══╝     ██║   ██║     ██╔══██║██╔══╝  ██╔══██╗
-███████╗██║██║           ██║     ███████╗   ██║   ╚██████╗██║  ██║███████╗██║  ██║
-╚══════╝╚═╝╚═╝           ╚═╝     ╚══════╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝"""
+import zfASCII
 
 class ZipFetcher:
     
@@ -23,7 +17,7 @@ class ZipFetcher:
 
         self.console = Console()
 
-        self.logo = LOGO
+        self.logo = zfASCII.LOGO
 
         # regex mønstre
         self.zip_pat = r"[A-Za-z_0-9]+\.zip" # .zip-fil
@@ -31,7 +25,7 @@ class ZipFetcher:
         self.invalid_chars = r'[<>:"|?\*]' # ugyldige tegn for mappenavn ## TODO endre regler for ':' ?
     
 
-    def _end(self) -> None:
+    def _terminate_program(self) -> None:
         input("\nTrykk enter for å avslutte... ")
         exit(0)
 
@@ -47,19 +41,20 @@ class ZipFetcher:
 
         if q == "y":
             self.console.print()
-            self._core()
+            self._process_downloads()
         else:
-            self._end()
+            self._terminate_program()
         
     
     def _validate_input(self,input:str, regex:str) -> bool:
+        # NOTE uklar logikk TODO fikse/endre?
         if re.search(regex, input) is None:
             return True
         else:
             return False
     
 
-    def _summary_confirm(self) -> bool:
+    def _summary_confirm(self, url:str, n_files:int, out_dir:str, cleanup:bool) -> bool:
         # oppsummering og bekreftelse
         summary = Table(title="Oppsummering:", box=box.DOUBLE)
 
@@ -68,24 +63,24 @@ class ZipFetcher:
         summary.show_header = False
         summary.show_lines = True
 
-        summary.add_row("Henter fra:", self.url)
-        summary.add_row("Antall arkiver funnet:", f"{self.n_files}")
-        summary.add_row("Output-mappe:", self.out_dir)
-        summary.add_row("Behold arkiv-filer?", f"{'NEI' if self.cleanup else 'JA'}")
+        summary.add_row("Henter fra:", url)
+        summary.add_row("Antall arkiver funnet:", f"{n_files}")
+        summary.add_row("Output-mappe:", out_dir)
+        summary.add_row("Behold arkiv-filer?", f"{'NEI' if cleanup else 'JA'}")
 
         self.console.print(summary)
 
         return True if Prompt.ask("Start nedlasting?", choices=["y","n"], default="y") == "y" else False
     
 
-    def _core(self) -> None:
-        self.url = Prompt.ask("Angi url å hente .zip-arkiver fra", default="https://www.uio.no/studier/emner/matnat/ifi/IN3050/v25/weekly-exercises/") # henter url fra bruker
+    def _process_downloads(self) -> None:
+        url = Prompt.ask("Angi url å hente .zip-arkiver fra", default="https://www.uio.no/studier/emner/matnat/ifi/IN3050/v25/weekly-exercises/") # henter url fra bruker
 
-        link_pat = f"{self.url}{self.zip_pat}" if self.url[-1] == "/" else f"{self.url}/{self.zip_pat}" # .zip-fil lenker
+        link_pat = f"{url}{self.zip_pat}" if url[-1] == "/" else f"{url}/{self.zip_pat}" # .zip-fil lenker
 
         # leser inn kildekode til angitt nettside/url
         try:
-            with urllib.request.urlopen(self.url) as page:
+            with urllib.request.urlopen(url) as page:
                 self.html = page.read().decode("utf8")
         except:
             self._error("Ugyldig url.")
@@ -95,23 +90,24 @@ class ZipFetcher:
         links = re.findall(link_pat, self.html)
         links = set(links)
 
-        self.out_dir = Prompt.ask("Angi mappe for output (enter for å bruke gjeldende mappe)", default=".")
+        out_dir = Prompt.ask("Angi mappe for output (enter for å bruke gjeldende mappe)", default=".")
 
-        while not self._validate_input(self.out_dir, self.invalid_chars):
-            self.out_dir = Prompt.ask(f"Unngå å bruke følgende symboler: [#fc0303]<, >, :, \", |, ?, *[/#fc0303]\nAngi mappe for output (enter for å bruke gjeldende mappe)", default=".")
+        while not self._validate_input(out_dir, self.invalid_chars):
+            out_dir = Prompt.ask(f"Unngå å bruke følgende symboler: [#fc0303]<, >, :, \", |, ?, *[/#fc0303]\nAngi mappe for output (enter for å bruke gjeldende mappe)", default=".")
         
         clean = Prompt.ask("Beholde .zip-arkiver?", choices=["y","n"], default="n")
 
-        self.cleanup = False if clean == "y" else True
+        cleanup = False if clean == "y" else True
 
         # antall .zip-lenker funnet
-        self.n_files = len(links)
+        n_files = len(links)
 
         # verifiserer at det er minst et arkiv på angitt url
-        if not self.n_files > 0:
-            self._error(f"Fant ingen .zip-arkiver på: {self.url}")
+        if not n_files > 0:
+            self._error(f"Fant ingen .zip-arkiver på: {url}")
+            return
 
-        confirm = self._summary_confirm()
+        confirm = self._summary_confirm(url, n_files, out_dir, cleanup)
 
         if confirm == False:
             self.console.print("\n[bold green blink]Avbrutt av bruker![/bold green blink]")
@@ -119,7 +115,7 @@ class ZipFetcher:
             self._retry()
 
         else:
-            for link in track(links,f"Laster ned {self.n_files} arkiver og ekstraherer til '{self.out_dir}'..."):
+            for link in track(links,f"Laster ned {n_files} arkiver og ekstraherer til '{out_dir}'..."):
                 # isolerer filnavn fra lenke
                 name = re.search(self.zip_pat, link).group()
 
@@ -130,10 +126,10 @@ class ZipFetcher:
                 with zipfile.ZipFile(name, 'r') as zip_ref:
                     subfolder = re.search(self.subfold_pat, name).group()
                     
-                    zip_ref.extractall(f"{self.out_dir}/{subfolder}")
+                    zip_ref.extractall(f"{out_dir}/{subfolder}")
 
                 # hvis valgt: sletter zipfil etter ekstraksjon
-                if self.cleanup:
+                if cleanup and os.path.exists(name):
                     os.remove(name)
 
             self.console.print("\n[bold green blink]Nedlasting/ekstraksjon fullført![/bold green blink]")
@@ -147,9 +143,9 @@ class ZipFetcher:
 
         self.console.print(Panel(self.logo, box=box.DOUBLE, padding=(2,sidepad), style="#90fc03", width=W, expand=False,subtitle="[#90fc03]av [u link https://talleraas.it/]talleraas.it[/u link https://talleraas.it/][/#90fc03]"))
 
-        self._core()
+        self._process_downloads()
 
-        self._end()
+        self._terminate_program()
 
 
 ZipFetcher().main()
